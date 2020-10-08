5fa2f53edcf7af4578b60d9324bf7ec7d97ea9c4
202004g07 <66996129+202004g07@users.noreply.github.com>
2020-10-08 18:27:47 +0900

    GameOver時の演出、TimeScaleを徐々に減少する

---
+++ Assets/SlowTest.cs
@@ -0,0 +1,29 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlowTest : MonoBehaviour
+{
+    [SerializeField] private Ease ease = Ease.InOutExpo;
+    [SerializeField] private float time = 2.0f;
+    [SerializeField] private float EndTimeScale = 0;
+    public void SlowTime()
+    {
+        DOTween.To(
+            () => Time.timeScale,
+            num => Time.timeScale = num,
+            EndTimeScale,
+            time)
+            .SetEase(ease);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            SlowTime();
+        }
+    }
+}
