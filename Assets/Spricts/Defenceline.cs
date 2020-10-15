cf8f0f0e6772f795908f9b5d8b143ad1dbf378e5
Siki02 <56345423+Siki02@users.noreply.github.com>
2020-10-15 18:12:24 +0900

    耐久度のあるオブジェクトを守るようなゲーム性に調整

---
+++ Assets/Spricts/Defenceline.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class Defenceline
+{
+    private static Defenceline instance;
+    private Defenceline(){}
+    public static Defenceline Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new Defenceline();
+            }
+            return instance;
+        }
+    }
+    public float Durability;
+}
